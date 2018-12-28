d1691c6e8ad84c96b7be1e84059862d39882023c
Iris Ng <irisngwl@gmail.com>
2018-12-28 17:45:30 +0800

    Initial Deployment

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ mongoose.connect(
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+
 app.use(express.static(path.join(__dirname, 'client', 'build')));
-app.use(require('dotenv').config());
 
 //-- ROUTES
 

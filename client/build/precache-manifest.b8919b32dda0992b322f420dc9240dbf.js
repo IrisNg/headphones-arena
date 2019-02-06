4bf519f05b697329af38dc5007035c61b5c4b666
Iris Ng <irisngwl@gmail.com>
2019-02-06 15:06:40 +0800

    Try to fix

---
+++ client/build/precache-manifest.b8919b32dda0992b322f420dc9240dbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91170373e1f700bd1188",
+    "url": "/static/css/main.08603324.chunk.css"
+  },
+  {
+    "revision": "91170373e1f700bd1188",
+    "url": "/static/js/main.91170373.chunk.js"
+  },
+  {
+    "revision": "d7d8567affb9124b973e",
+    "url": "/static/js/1.d7d8567a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5f6691086400c690dcd56aa5aa2051d5",
+    "url": "/index.html"
+  }
+];

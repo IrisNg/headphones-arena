87dd24a796fd20c1dabfbaec1f206ab7e6fb8824
Iris Ng <irisngwl@gmail.com>
2019-02-19 18:33:00 +0800

    Finish skeleton CSS for blacksmith

--- client/src/components/Blacksmith/Blacksmith.js
+++ client/src/components/Blacksmith/Blacksmith.js
@@ -49,8 +49,13 @@ class Blacksmith extends React.Component {
       const { featuredVideo } = this.props;
       return (
          <div className="blacksmith">
-            {/* <div className="blacksmith__horizontal-line" /> */}
             <div className="blacksmith__videos">{this.renderListOfVideos()}</div>
+            <div
+               className="blacksmith__youtube-button"
+               onClick={() => window.open('https://www.youtube.com/channel/UCNlglDSoE8oSUgxqhSnDdXw/videos')}
+            >
+               <i className="fas fa-caret-right" />
+            </div>
             <div className="blacksmith__page-title">BLACKSMITH</div>
             <FeaturedVideo featuredVideo={featuredVideo} />
             <div className="blacksmith__vertical-line" />
